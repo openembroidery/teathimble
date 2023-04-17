74eedd15bf957d2d1da4e4689424376020be8770
Gian Hug <gihug@student.ethz.ch>
2023-04-17 16:03:24 +0200

    fixed timer0 init for non atmega32/16 uCs

--- sensors_control.c
+++ sensors_control.c
@@ -13,69 +13,70 @@ int16_t margin_max_speed = DEFAULT_MOTOR_SPEED;
 /// Init INT0 and INT1 interrupts for optical sensors, init pwm and timers for dc motor speed controller
 void sensing_init()
 {
-	PULL_OFF(PD2); 
+	PULL_OFF(PD2);
     PULL_OFF(ENCODER_PIN_A);
     PULL_OFF(ENCODER_PIN_B);
     SET_INPUT(PD2);
     SET_INPUT(ENCODER_PIN_A);
     SET_INPUT(ENCODER_PIN_B);
-    
+
   #if defined (__AVR_ATmega32U4__) || defined (__AVR_ATmega32__) || \
             defined (__AVR_ATmega16__)
-	// externally trigged interrupts setup    
+	// externally trigged interrupts setup
     MCUCR |= MASK(ISC00) | MASK(ISC11) | MASK(ISC10); /* INT0 - any change, INT1 - raising edge */
     GICR |= MASK(INT0) | MASK(INT1);        /* enable INT0 and INT1 */
-    
+
     // setup PWM timers: fast PWM
 	// Warning: these are not consistent across all AVRs
 	TCCR0 = MASK(WGM01) | MASK(WGM00) | MASK(COM01);
 	// pwm frequency
 	#ifndef FAST_PWM
-		TCCR0 |= MASK(CS02); // F_CPU / 256   
+		TCCR0 |= MASK(CS02); // F_CPU / 256
 	#else
-		TCCR0 |=  MASK(CS01); // F_CPU / 8 
+		TCCR0 |=  MASK(CS01); // F_CPU / 8
 	#endif
-	
+
 	#define MOTOR_PWM OCR0
 	OCR0 = 0;
 	PB4_DDR = 0xFF;
 
 	// timer 1 is used for stepping
-	
+
     TCCR2 = MASK(CS20) | MASK(CS21) | MASK(CS22); // F_CPU / 256 / 1024
     OCR2 = 0;
     // enable timer overflow int
     TIMSK |= MASK(TOIE2);
   #else
-    // externally trigged interrupts setup    
+    // externally trigged interrupts setup
     EICRA |= MASK(ISC00) | MASK(ISC11) | MASK(ISC10); /* INT0 - any logic change INT1 - raising edge */
     EIMSK |= MASK(INT0) | MASK(INT1);        /* enable INT0 and INT1 */
-    
+
 	// timer 1 is used for stepping
-	
+
     // PWM frequencies in TCCR2B, see page 156 of the ATmega644 reference.
     TCCR2A = 0;
     TCCR2B = MASK(CS20) | MASK(CS21) | MASK(CS22); // F_CPU / 256 / 1024
-    
+
     OCR2A = 0;
     OCR2B = 0;
-    
+
     // pwm frequency
     #ifndef FAST_PWM
 		TCCR0B =  (TCCR0B & 0b11111000) | 0x04;
 	#else
 		TCCR0B =  (TCCR0B & 0b11111000) | 0x02;
 	#endif
-    
-    #define MOTOR_PWM OCR0A
-    OCR0A = 0;
-    OCR0B = 0;
-    PD6_DDR = 0xFF;
-    
+		// init of timer 0 for pwm motor control. ouput pin is PD6
+		TCCR0A = MASK(WGM01) | MASK(WGM00) | MASK(COM0A1);
+		#define MOTOR_PWM OCR0A
+		OCR0A = 0;
+		OCR0B = 0;
+		SET_OUTPUT(PD6);
+
     // enable timer overflow int
     TIMSK2 = MASK(TOIE2);
   #endif
-    
+
     error_speed = desired_speed = error_speed_sum = 0;
 	kp = KP_FACTOR;
 	ki = KI_FACTOR;
@@ -96,7 +97,7 @@ void set_dc_motor_speed_margin(int16_t value)
 		else
 			margin_max_speed = MIN_MOTOR_SPEED;
 	}
-	else 
+	else
 		margin_max_speed = MAX_MOTOR_SPEED;
 }
 
@@ -128,7 +129,7 @@ void update_dc_motor()
 		uint8_t time_interval = (F_CPU / 256 / 1024 / interrupt_ticks);
 		//calculate motor speed, unit is decimal rpm
 		int16_t pv_speed = ( 60 * (motor_pulses*POINT_SHIFT/PULSES_PER_TURN) * time_interval ) / POINT_SHIFT;
-  
+
 		interrupt_ticks = 0;
 		motor_pulses = 0;
 		error_speed = desired_speed - pv_speed;
@@ -148,7 +149,7 @@ void update_dc_motor()
 	// set new motor power
 	if (pwm_pulse < 255 && pwm_pulse > 0)
 	{
-		MOTOR_PWM = pwm_pulse; 
+		MOTOR_PWM = pwm_pulse;
 	}
    else
    {
@@ -158,11 +159,11 @@ void update_dc_motor()
 		}
 		else
 		{
-			MOTOR_PWM = 0; 
+			MOTOR_PWM = 0;
 		}
    }
 }
- 
+
 /// needle interrupt
 ISR(INT1_vect)
 {
@@ -208,14 +209,13 @@ ISR(INT1_vect)
 }
 
 /// motor rotary encoder interrupt for speed measurement
-ISR(INT0_vect) 
-{ 
+ISR(INT0_vect)
+{
 	++motor_pulses;
 }
- 
+
 /// timer tick interrupt
-ISR(TIMER2_OVF_vect) 
+ISR(TIMER2_OVF_vect)
 {
 	++interrupt_ticks;
-} 
- 
+}
